e0677c11580b09a898322b52d8dfa1f767fa4a84
nikssa <nikssa@gmail.com>
2024-05-29 01:40:11 +0200

    Code cleanup

--- src/components/AnalogClockSVG.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactComponent from '../assets/svg/clock.svg?react';
-// import ErrorBoundary from 'react-error-boundary'
-
-const AnalogClockSVG = () => {
-  return <ReactComponent width={200} />;
-};
-
-export default AnalogClockSVG;
